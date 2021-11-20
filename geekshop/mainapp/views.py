a1d5a8bb05b0f5ecaefe891e77a9d4cfb1286197
BandS-Y <yuverch@gmail.com>
2021-11-20 17:56:27 +0700

    Lesson_2 HW

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,7 +3,36 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title' : 'myshop',
+        'naviname': 'myshop',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'title' : 'myshop - Каталог',
+        'naviname': 'myshop',
+        'products':[
+            {'name' : 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
+             'about' : 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+            {'name': 'Синяя куртка The North Face', 'price': 23725,
+             'about': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
+             'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
+             'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
+             'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
+             'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+        ]
+    }
+    return render(request, 'mainapp/products.html', context)
+
+def base(request):
+    context = {
+        'naviname' : 'myshop',
+        'pagehead_store' : 'myshop',
+    }
+    return render(request, 'mainapp/base.html', context)
