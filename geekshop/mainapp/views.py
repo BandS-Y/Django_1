1bfb8f4f3e43cec34fd26295841c6ba8888a99fd
BandS-Y <yuverch@gmail.com>
2021-12-08 23:59:37 +0700

    Lesson_7 HW

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,63 +1,45 @@
 from django.shortcuts import render
-from .models import ProductCategory, Product
 
 import json
 import os
 
+from django.views.generic import DetailView
+
+from mainapp.models import Product, ProductCategory
+
 MODULE_DIR = os.path.dirname(__file__)
+
+
 # Create your views here.
 
 def index(request):
     context = {
-        'title' : 'myshop',
-        'naviname': 'myshop',
-    }
+        'title': 'Geekshop', }
     return render(request, 'mainapp/index.html', context)
 
+
 def products(request):
-    products = Product.objects.all()
-
-    # file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    # file_path = os.path.join(MODULE_DIR,'fixtures/goods.json')
     context = {
-        'title' : 'myshop - Каталог',
-        'naviname': 'myshop',
-        'products': products,
-        # 'products':[
-        #     {'name' : 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
-        #      'about' : 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-        #      'image' : 'vendor/img/products/Adidas-hoodie.png'},
-        #     {'name': 'Синяя куртка The North Face', 'price': 23725,
-        #      'about': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-        #      'image' : 'vendor/img/products/Blue-jacket-The-North-Face.png'},
-        #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
-        #      'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-        #      'image' : 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
-        #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
-        #      'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-        #      'image' : 'vendor/img/products/Black-Nike-Heritage-backpack.png'},
-        #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
-        #      'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-        #      'image' : 'vendor/img/products/Black-Dr-Martens-shoes.png'},
-        #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
-        #      'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-        #      'image' : 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
-        # ],
-        # 'menu' : [
-        #     {'point': 'Новинки'},
-        #     {'point': 'Одежда'},
-        #     {'point': 'Обувь'},
-        #     {'point': 'Аксессуары'},
-        #     {'point': 'Подарки'},
-        # ],
+        'title': 'Geekshop | Каталог',
     }
-    # context['products'] = json.load(open(file_path, encoding='utf-8'))
 
-    context['categorys'] = ProductCategory.objects.all()
+    # context['products'] = json.load(open(file_path,encoding='utf-8'))
+    context['products'] = Product.objects.all()
+    context['categories'] = ProductCategory.objects.all()
     return render(request, 'mainapp/products.html', context)
 
-def base(request):
-    context = {
-        'naviname' : 'myshop',
-        'pagehead_store' : 'myshop',
-    }
-    return render(request, 'mainapp/base.html', context)
+
+class ProductDetail(DetailView):
+    """
+    Контроллер вывода информации о продукте
+    """
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        return context
+
