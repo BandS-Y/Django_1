451086a6f8034d8316c931e9fe93f50b28f34234
BandS-Y <yuverch@gmail.com>
2021-12-08 23:31:47 +0700

    Lesson_6 ajx fin

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from mainapp.models import ProductCategory, Product
+
+# Register your models here.
+from mainapp.models import ProductCategory,Product
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
