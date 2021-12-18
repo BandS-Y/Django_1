e8109f47569b49db404556b68d21f4972d866f71
BandS-Y <yuverch@gmail.com>
2021-12-18 16:59:29 +0700

    k_2_lesson_2 tune data users

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -3,12 +3,12 @@
 from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.views.generic import FormView, UpdateView
 
-from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm, UserProfileEditForm
 from django.urls import reverse, reverse_lazy
 
 from baskets.models import Basket
@@ -75,6 +75,12 @@ class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
     # success_message = 'OK'
     title = 'MyShop - Профиль'
 
+    def post(self, request, *args, **kwargs):
+        form = UserProfilerForm(data=request.POST, files=request.FILES, instance=request.user)
+        profile_form = UserProfileEditForm(request.POST, instance=request.user.userprofile)
+        if form.is_valid() and profile_form.is_valid():
+            form.save()
+        return redirect(self.success_url)
 
     def form_valid(self, form):
         messages.set_level(self.request,messages.SUCCESS)
@@ -85,10 +91,10 @@ class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
     def get_object(self, *args, **kwargs):
         return get_object_or_404(User, pk=self.request.user.pk)
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(ProfileFormView, self).get_context_data(**kwargs)
-    #     context['baskets'] = Basket.objects.filter(user=self.request.user)
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(ProfileFormView, self).get_context_data(**kwargs)
+        context['profile'] = UserProfileEditForm(instance=self.request.user.userprofile)
+        return context
 
 class Logout(LogoutView):
     template_name = "mainapp/index.html"
