87fe0dbff6b8e17c087def13a957ea1a3e7652e4
BandS-Y <yuverch@gmail.com>
2021-12-25 09:58:51 +0700

    k_2_lesson_3 begin + +

--- geekshop/authapp/pipelines.py
+++ geekshop/authapp/pipelines.py
@@ -50,7 +50,7 @@ def save_user_profile(backend, user, response, *args, **kwargs):
         user.image = path_photo
 
     if data['personal']['langs']:
-        user.userprofile.langs = data['personal']['langs'][0] if len(data['personal']['langs'][0]) > 0 else 'EN'
+        user.userprofile.langs = data['personal']['langs'][0] if len(data['personal']['langs'][0]) > 0 else 'RU'
 
 
 
